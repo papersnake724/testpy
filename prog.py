f1a62f7aea12a688962b47619429f7ca77a54753
papersnake724 <papersnake724@users.noreply.github.com>
2015-04-25 18:42:04 +0600

    del

--- prog.py
+++ prog.py
@@ -5,12 +5,10 @@ print ':)'
 print 2+2
 print 2+3
 print 2*9
-print 9/3
-<<<<<<< HEAD
+
 print "abc"
 print 1+1
 print "from brunch another"
 =======
 print "this from new brunch"
-print "done"
->>>>>>> origin/my_new_branch
+
